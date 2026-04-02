5dd89329070c24c6dbf6199a018cc511753aed05
prateek <prateekj848@gmail.com>
2026-04-03 01:10:21 +0530

    Error class changes

--- src/main/java/com/qa/opencart/errors/AppErrors.java
+++ src/main/java/com/qa/opencart/errors/AppErrors.java
@@ -2,5 +2,5 @@ package com.qa.opencart.errors;
 
 public class AppErrors {
  
-   public static final String INVALID_BROWSER_MSG="Please pass the right browser such as chrome, edge, firefox and edge";
+   public static final String INVALID_BROWSER_MSG="Please pass the right browser such as chrome, edge,firefox and edge";
 }
